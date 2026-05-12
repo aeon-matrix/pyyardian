d08d8dbde13a332994e5f5633cff8ffa7aef5620
Yardian Support <contact@aeon-matrix.com>
2026-05-12 17:08:08 +0800

    update version to v1.3.0

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyyardian",
-    version="1.2.0",
+    version="1.3.0",
     license="MIT",
-    author="Marty Sun",
-    author_email="marty.sun@aeonmatrix.com",
+    author="Yardian Support",
+    author_email="contact@aeonmatrix.com",
     description="A module for interacting with the Yardian irrigation controller",
     long_description=" Python module for interacting with the Yardian irrigation controller. This module communicates directly towards the IP address of the Yardian device.",
     long_description_content_type="text/markdown",
-    url="https://github.com/h3l1o5/pyyardian",
+    url="https://github.com/aeon-matrix/pyyardian",
     install_requires=["aiohttp"],
     packages=find_packages()
 )
